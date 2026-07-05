e1e17cb1b8deb2cf006279c4d1d39c1904daa7af
dora dora <dora_1324@gmail.com>
2026-07-05 01:19:30 +0100

    Update: checkout journey test

--- pages/ProductPage.ts
+++ pages/ProductPage.ts
@@ -13,6 +13,7 @@ export class ProductPage {
 
   async goto() {
     await this.page.getByRole("link", { name: " Products" }).click();
+
     const adPopup = this.page.locator('iframe[name="aswift_3"]');
     if (await adPopup.isVisible()) {
       await adPopup
@@ -40,17 +41,55 @@ export class ProductPage {
   }
 
   async addProductToCart() {
-    await this.page.getByText("Add to cart").nth(1).click();
-    // await this.page.locator(".productinfo").first().hover();
-    // await this.page.locator(".overlay-content .add-to-cart").first().click();
-    // await this.page.getByRole("link", { name: "View Cart" }).click();
+    await this.page.locator(".productinfo").first().hover();
+    await this.page.locator(".overlay-content .add-to-cart").first().click();
+  }
+
+  async viewCart() {
+    await this.page.getByRole("link", { name: "View Cart" }).click();
   }
 
   async viewProductDetails() {
     await this.page
       .getByRole("link", { name: " View Product" })
-      .nth(1)
+      .first()
       .click();
-    // await this.page.getByRole("button", { name: " Add to cart" }).click();
+  }
+
+  async addToCartFromProductDetails() {
+    await this.page
+      .getByRole("link", { name: " View Product" })
+      .first()
+      .click();
+    await this.page.getByRole("button", { name: " Add to cart" }).click();
+  }
+
+  async deleteProductFromCart() {
+    await this.page.getByRole("link", { name: " Cart" }).click();
+    if (await this.page.locator(".cart_quantity_delete").isVisible()) {
+      await this.page.locator(".cart_quantity_delete").click();
+    }
+  }
+
+  async proceedToCheckout() {
+    await this.page.getByText("Proceed To Checkout").click();
+  }
+
+  async placeOrder(
+    name: string,
+    cardNumber: string,
+    cvc: string,
+    expiryMonth: string,
+    expiryYear: string,
+  ) {
+    await this.page.getByRole("link", { name: "Place Order" }).click();
+    await this.page.locator('input[name="name_on_card"]').fill(name);
+    await this.page.locator('input[name="card_number"]').fill(cardNumber);
+    await this.page.getByRole("textbox", { name: "ex." }).fill(cvc);
+    await this.page.getByRole("textbox", { name: "MM" }).fill(expiryMonth);
+    await this.page.getByRole("textbox", { name: "YYYY" }).fill(expiryYear);
+    await this.page
+      .getByRole("button", { name: "Pay and Confirm Order" })
+      .click();
   }
 }
--- tests/checkout.spec.ts
+++ tests/checkout.spec.ts
@@ -1,8 +1,15 @@
 import { test, expect } from "@playwright/test";
 import { HomePage } from "../pages/HomePage";
 import { ProductPage } from "../pages/ProductPage";
+import { LoginPage } from "../pages/LoginPage";
+
+const TEST_LOGIN_EMAIL = process.env.TEST_VALID_EMAIL ?? "";
+const TEST_PASSWORD = process.env.TEST_VALID_PASSWORD ?? "";
 
 test.beforeEach(async ({ page }) => {
+  await page.route(/(googlesyndication|doubleclick|adsbygoogle)/, (route) =>
+    route.abort(),
+  );
   const homePage = new HomePage(page);
   await homePage.goto();
   await homePage.cookieConsent();
@@ -21,5 +28,70 @@ test("add product to cart", async ({ page }) => {
   await productPage.addProductToCart();
   await expect(page.locator("body")).toContainText(
     "Your product has been added to cart.",
+  );
+});
+
+test("add product to cart in product details page", async ({ page }) => {
+  const productPage = new ProductPage(page);
+  await productPage.goto();
+  await productPage.addToCartFromProductDetails();
+  await expect(page).toHaveURL(/.*product_details\/1/);
+  await expect(page.locator("body")).toContainText(
+    "Your product has been added to cart.",
+  );
+});
+
+test("view cart after adding product", async ({ page }) => {
+  const productPage = new ProductPage(page);
+  await productPage.goto();
+  await productPage.addProductToCart();
+  await productPage.viewCart();
+  await expect(page).toHaveURL(/.*view_cart/);
+  await expect(page.locator("body")).toContainText("Proceed To Checkout");
+});
+
+test("delete product from cart", async ({ page }) => {
+  const productPage = new ProductPage(page);
+  await productPage.goto();
+  await productPage.deleteProductFromCart();
+
+  await expect(page.locator("body")).toContainText("Cart is empty!");
+});
+
+test("proceed to checkout after login", async ({ page }) => {
+  const loginPage = new LoginPage(page);
+  const productPage = new ProductPage(page);
+  await loginPage.navigateFromHomepage();
+  await loginPage.fillForm(TEST_LOGIN_EMAIL, TEST_PASSWORD);
+  await loginPage.submit();
+  await productPage.goto();
+  await productPage.addProductToCart();
+  await productPage.viewCart();
+  await expect(page.locator("body")).toContainText("Proceed To Checkout");
+  await productPage.proceedToCheckout();
+  await expect(page).toHaveURL(/.*checkout/);
+  await productPage.placeOrder(
+    "Yannes Test",
+    "4111111111111111",
+    "123",
+    "07",
+    "2026",
+  );
+  await expect(page).toHaveURL(/.*payment_done/);
+  await expect(page.locator("body")).toContainText(
+    "Congratulations! Your order has been confirmed!",
+  );
+});
+
+test("proceed to checkout without login", async ({ page }) => {
+  const productPage = new ProductPage(page);
+  await productPage.goto();
+  await productPage.addProductToCart();
+  await productPage.viewCart();
+  await expect(page.locator("body")).toContainText("Proceed To Checkout");
+  await productPage.proceedToCheckout();
+  await expect(page).toHaveURL(/.*view_cart/);
+  await expect(page.locator("body")).toContainText(
+    "Register / Login account to proceed on checkout.",
   );
 });
